73941ee82a40d48174d37ba28c39344ecd719503
prangyan das <prangyan.das@byjus.com>
2022-02-24 21:49:40 +0530

    movies-app

---
+++ server/models/Movie.js
@@ -0,0 +1,39 @@
+const { Sequelize, DataTypes } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+    const Movie = sequelize.define('Movie', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        poster: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        rating: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            allowNull: false
+        }
+    }, {
+        freezeTableName: true,
+        timestamps: true,
+        underscored: true,
+        tableName: 'moviesdata',
+        schema: 'public'
+    });
+
+    return Movie;
+}
